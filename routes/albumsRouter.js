4d9e51562f56466d8fcc12b037a680711f5d03f5
Yash Raj <yashrj130@gmail.com>
2019-08-20 17:08:45 +0530

    Add files via upload

---
+++ routes/albumsRouter.js
@@ -0,0 +1,11 @@
+// All /albums routes
+
+const express = require('express');
+const router = express.Router();
+
+const { getAllAlbums } = require('../controllers/albumsController');
+
+// Note: this route is actually /albums due to our index.js setup
+router.get('/', getAllAlbums);
+
+module.exports = router;
